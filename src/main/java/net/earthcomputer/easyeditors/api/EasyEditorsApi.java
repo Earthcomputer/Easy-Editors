9232d0890c4eb9216e8351d6b463c7d957da5146
Earthcomputer <burtonjae@hotmail.co.uk>
2016-01-16 13:44:12 +0000

    Add NBTTagHandler, creating a way for the user to easily manipulate NBT tags

--- src/main/java/net/earthcomputer/easyeditors/api/EasyEditorsApi.java
+++ src/main/java/net/earthcomputer/easyeditors/api/EasyEditorsApi.java
@@ -2,6 +2,14 @@ package net.earthcomputer.easyeditors.api;
 
 import java.lang.reflect.Method;
 
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IChatComponent;
+
 /**
  * Static utilities involving the Easy Editors API.
  * 
@@ -23,16 +31,21 @@ public class EasyEditorsApi {
 
 	private static Class<?> cls_EasyEditors;
 	private static Class<?> cls_ICommandSyntax;
+	private static Class<?> cls_NBTTagHandler;
 
 	private static Method md_ICommandSyntax_registerCommandSyntax;
+	private static Method md_NBTTagHandler_registerHandler;
 
 	static {
 		try {
 			cls_EasyEditors = Class.forName("net.earthcomputer.easyeditors.EasyEditors");
 			cls_ICommandSyntax = Class.forName("net.earthcomputer.easyeditors.gui.command.ICommandSyntax");
+			cls_NBTTagHandler = Class.forName("net.earthcomputer.easyeditors.gui.command.NBTTagHandler");
 
 			md_ICommandSyntax_registerCommandSyntax = cls_ICommandSyntax.getMethod("registerCommandSyntax",
 					String.class, Class.class);
+			md_NBTTagHandler_registerHandler = cls_NBTTagHandler.getMethod("registerHandler", String.class,
+					Predicate.class, Class.class);
 		} catch (Exception e) {
 		}
 	}
@@ -49,6 +62,9 @@ public class EasyEditorsApi {
 	 * 
 	 * @param commandName
 	 * @param className
+	 * 
+	 * @see net.earthcomputer.easyeditors.gui.command.ICommandSyntax#registerCommandSyntax(String,
+	 *      Class) ICommandSyntax.registerCommandSyntax(String, Class)
 	 */
 	public static void registerCommandSyntax(String commandName, String className) {
 		if (cls_EasyEditors == null)
@@ -56,6 +72,95 @@ public class EasyEditorsApi {
 		try {
 			md_ICommandSyntax_registerCommandSyntax.invoke(null, commandName, Class.forName(className));
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * A safe way of registering an NBTTagHandler for TileEntities without
+	 * raising a {@link NoClassDefFoundError} if the Easy Editors mod is not
+	 * present. Modders should ensure that the referred class in
+	 * handlerClassName is not referenced directly in code where one cannot be
+	 * sure whether Easy Editors exists. Modders should also make sure that the
+	 * referenced class is a subclass of
+	 * {@link net.earthcomputer.easyeditors.gui.command.NBTTagHandler
+	 * NBTTagHandler}
+	 * 
+	 * @param tileEntity
+	 * @param handlerClassName
+	 * 
+	 * @see net.earthcomputer.easyeditors.gui.command.NBTTagHandler#registerTileEntityHandler(Class,
+	 *      Class) NBTTagHandler.registerTileEntityHandler(Class, Class)
+	 */
+	public static void registerTileEntityNBTHandler(Class<? extends TileEntity> tileEntity, String handlerClassName) {
+		registerNBTHandler("tileEntity", Predicates.assignableFrom(tileEntity), handlerClassName);
+	}
+
+	/**
+	 * A safe way of registering an NBTTagHandler for ItemStacks without raising
+	 * a {@link NoClassDefFoundError} if the Easy Editors mod is not present.
+	 * Modders should ensure that the referred class in handlerClassName is not
+	 * referenced directly in code where one cannot be sure whether Easy Editors
+	 * exists. Modders should also make sure that the referenced class is a
+	 * subclass of
+	 * {@link net.earthcomputer.easyeditors.gui.command.NBTTagHandler
+	 * NBTTagHandler}
+	 * 
+	 * @param itemStackPredicate
+	 * @param handlerClassName
+	 * 
+	 * @see net.earthcomputer.easyeditors.gui.command.NBTTagHandler#registerItemStackHandler(Predicate,
+	 *      Class) NBTTagHandler.registerItemStackHandler(Predicate, Class)
+	 */
+	public static void registerItemStackNBTHandler(Predicate<ItemStack> itemStackPredicate, String handlerClassName) {
+		registerNBTHandler("itemStack", itemStackPredicate, handlerClassName);
+	}
+
+	/**
+	 * A safe way of registering an NBTTagHandler for Entities without raising a
+	 * {@link NoClassDefFoundError} if the Easy Editors mod is not present.
+	 * Modders should ensure that the referred class in handlerClassName is not
+	 * referenced directly in code where one cannot be sure whether Easy Editors
+	 * exists. Modders should also make sure that the referenced class is a
+	 * subclass of
+	 * {@link net.earthcomputer.easyeditors.gui.command.NBTTagHandler
+	 * NBTTagHandler}
+	 * 
+	 * @param entity
+	 * @param handlerClassName
+	 * 
+	 * @see net.earthcomputer.easyeditors.gui.command.NBTTagHandler#registerEntityHandler(Class,
+	 *      Class) NBTTagHandler.registerEntityHandler(Class, Class)
+	 */
+	public static void registerEntityNBTHandler(Class<? extends Entity> entity, String handlerClassName) {
+		registerNBTHandler("entity", Predicates.assignableFrom(entity), handlerClassName);
+	}
+
+	/**
+	 * A safe way of registering an NBTTagHandler for custom purposes without
+	 * raising a {@link NoClassDefFoundError} if the Easy Editors mod is not
+	 * present. Modders should ensure that the referred class in
+	 * handlerClassName is not referenced directly in code where one cannot be
+	 * sure whether Easy Editors exists. Modders should also make sure that the
+	 * referenced class is a subclass of
+	 * {@link net.earthcomputer.easyeditors.gui.command.NBTTagHandler
+	 * NBTTagHandler}
+	 * 
+	 * @param handlerType
+	 * @param predicate
+	 * @param handlerClassName
+	 * 
+	 * @see net.earthcomputer.easyeditors.gui.command.NBTTagHandler#registerHandler(String,
+	 *      Predicate, Class) NBTTagHandler.registerTileEntityHandler(String,
+	 *      Predicate, Class)
+	 */
+	public static void registerNBTHandler(String handlerType, Predicate<?> predicate, String handlerClassName) {
+		if (cls_EasyEditors == null)
+			return;
+		try {
+			md_NBTTagHandler_registerHandler.invoke(null, handlerType, predicate, Class.forName(handlerClassName));
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 
---
+++ src/main/java/net/earthcomputer/easyeditors/gui/command/NBTTagHandler.java
@@ -0,0 +1,212 @@
+package net.earthcomputer.easyeditors.gui.command;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotVerticalArrangement;
+import net.earthcomputer.easyeditors.gui.command.slot.IGuiCommandSlot;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IChatComponent;
+
+/**
+ * A class which allows you to create user-friendly representations of NBT tags
+ * 
+ * @author Earthcomputer
+ *
+ */
+public abstract class NBTTagHandler {
+
+	private static final Map<String, Map<Predicate<?>, Class<? extends NBTTagHandler>>> handlers = Maps.newHashMap();
+
+	/**
+	 * Sets up the command slots that can be used to edit the handled part of
+	 * the NBT tag. The returned array may be nested inside a
+	 * {@link CommandSlotVerticalArrangement} if necessary
+	 * 
+	 * @return
+	 */
+	public abstract IGuiCommandSlot[] setupCommandSlot();
+
+	/**
+	 * Read from an {@link NBTTagCompound} and inject into the command slots
+	 * 
+	 * @param nbt
+	 */
+	public abstract void readFromNBT(NBTTagCompound nbt);
+
+	/**
+	 * Read from the command slots and inject into the NBT
+	 * 
+	 * @param nbt
+	 */
+	public abstract void writeToNBT(NBTTagCompound nbt);
+
+	/**
+	 * Registers an NBTTagHandler which deals with NBT tags specific to the
+	 * given class of TileEntity. Modders should instead use
+	 * {@link net.earthcomputer.easyeditors.api.EasyEditorsApi#registerTileEntityNBTHandler(Class, String)
+	 * EasyEditorsApi.registerTileEntityNBTHandler(Class, String)}
+	 * 
+	 * @param tileEntity
+	 * @param handler
+	 */
+	public static void registerTileEntityHandler(Class<? extends TileEntity> tileEntity,
+			Class<? extends NBTTagHandler> handler) {
+		registerHandler("tileEntity", Predicates.assignableFrom(tileEntity), handler);
+	}
+
+	/**
+	 * Registers an NBTTagHandler which deals with NBT tags in item stacks, when
+	 * your item contains custom information (like a book). Modders should
+	 * instead use
+	 * {@link net.earthcomputer.easyeditors.api.EasyEditorsApi#registerItemStackNBTHandler(Predicate, String)
+	 * EasyEditorsApi.registerItemStackNBTHandler(Predicate, String)}
+	 * 
+	 * @param stackPredicate
+	 * @param handler
+	 */
+	public static void registerItemStackHandler(Predicate<ItemStack> stackPredicate,
+			Class<? extends NBTTagHandler> handler) {
+		registerHandler("itemStack", stackPredicate, handler);
+	}
+
+	/**
+	 * Registers an NBTTagHandler which deals with NBT tags specific to the
+	 * given class of Entity. Modders should instead use
+	 * {@link net.earthcomputer.easyeditors.api.EasyEditorsApi#registerEntityNBTHandler(Class, String)
+	 * EasyEditorsApi.registerEntityNBTHandler(Class, String)}
+	 * 
+	 * @param entity
+	 * @param handler
+	 */
+	public static void registerEntityHandler(Class<? extends Entity> entity, Class<? extends NBTTagHandler> handler) {
+		registerHandler("entity", Predicates.assignableFrom(entity), handler);
+	}
+
+	/**
+	 * Registers a custom NBTTagHandler which deals with NBT tags in custom
+	 * scenarios, with the handlerType parameter representing the ID of the
+	 * custom scenario. Modders should instead use
+	 * {@link net.earthcomputer.easyeditors.api.EasyEditorsApi#registerNBTHandler(String, Predicate, String)
+	 * EasyEditorsApi.registerNBTHandler(String, Predicate, String)}
+	 * 
+	 * @param handlerType
+	 * @param predicate
+	 * @param handler
+	 */
+	public static void registerHandler(String handlerType, Predicate<?> predicate,
+			Class<? extends NBTTagHandler> handler) {
+		if (!handlers.containsKey(handlerType))
+			handlers.put(handlerType, new HashMap<Predicate<?>, Class<? extends NBTTagHandler>>());
+		handlers.get(handlerType).put(predicate, handler);
+	}
+
+	/**
+	 * Constructs a list of new NBTTagHandlers for the given class of TileEntity
+	 * 
+	 * @param tileEntity
+	 * @return
+	 */
+	public static List<NBTTagHandler> constructTileEntityHandlers(Class<? extends TileEntity> tileEntity) {
+		return constructHandlers("tileEntity", tileEntity);
+	}
+
+	/**
+	 * Constructs a list of new NBTTagHandlers for the given ItemStack
+	 * 
+	 * @param itemStack
+	 * @return
+	 */
+	public static List<NBTTagHandler> constructItemStackHandlers(ItemStack itemStack) {
+		return constructHandlers("itemStack", itemStack);
+	}
+
+	/**
+	 * Constructs a list of new NBTTagHandlers for the given class of Entity
+	 * 
+	 * @param entity
+	 * @return
+	 */
+	public static List<NBTTagHandler> constructEntityHandlers(Class<? extends Entity> entity) {
+		return constructHandlers("entity", entity);
+	}
+
+	/**
+	 * Constructs a list of new NBTTagHandlers for the given custom scenario,
+	 * with the handlerType parameter representing the ID of the custom
+	 * scenario. If specifier is null, this method returns all the
+	 * NBTTagHandlers assigned to this type of scenario
+	 * 
+	 * @param handlerType
+	 * @param specifier
+	 * @return
+	 */
+	public static List<NBTTagHandler> constructHandlers(String handlerType, Object specifier) {
+		List<NBTTagHandler> handlers = Lists.newArrayList();
+		if (!NBTTagHandler.handlers.containsKey(handlerType))
+			return handlers;
+		if (specifier == null) {
+			for (Class<? extends NBTTagHandler> handler : NBTTagHandler.handlers.get(handlerType).values()) {
+				try {
+					handlers.add(handler.getConstructor().newInstance());
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} else {
+			for (Map.Entry<Predicate<?>, Class<? extends NBTTagHandler>> entry : NBTTagHandler.handlers.get(handlerType)
+					.entrySet()) {
+				if (((Predicate<Object>) entry.getKey()).apply(specifier)) {
+					try {
+						handlers.add(entry.getValue().getConstructor().newInstance());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return handlers;
+	}
+
+	/**
+	 * Returns an {@link IGuiCommandSlot} which will be the user interface to
+	 * the NBT tags. The NBTTagHandlers must be preconstructed by using methods
+	 * of this class
+	 * 
+	 * @param nbtHandlers
+	 * @return
+	 */
+	public static IGuiCommandSlot setupCommandSlot(List<NBTTagHandler> nbtHandlers) {
+		List<IGuiCommandSlot> slots = Lists.newArrayList();
+		for (NBTTagHandler handler : nbtHandlers) {
+			for (IGuiCommandSlot slot : handler.setupCommandSlot()) {
+				slots.add(slot);
+			}
+		}
+		return slots.isEmpty() ? null
+				: (slots.size() == 1 ? slots.get(0)
+						: new CommandSlotVerticalArrangement(slots.toArray(new IGuiCommandSlot[slots.size()])));
+	}
+
+	public static void readFromNBT(NBTTagCompound nbt, List<NBTTagHandler> nbtHandlers) {
+		for (NBTTagHandler handler : nbtHandlers) {
+			handler.readFromNBT(nbt);
+		}
+	}
+
+	public static void writeToNBT(NBTTagCompound nbt, List<NBTTagHandler> nbtHandlers) {
+		for (NBTTagHandler handler : nbtHandlers) {
+			handler.writeToNBT(nbt);
+		}
+	}
+
+}
