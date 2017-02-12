cf41923fcf9f49f51e99074822aba8842907e40a
Earthcomputer <burtonjae@hotmail.co.uk>
2017-02-12 14:28:38 +0000

    toggledownfall syntax

--- src/main/java/net/earthcomputer/easyeditors/gui/command/syntax/CommandSyntax.java
+++ src/main/java/net/earthcomputer/easyeditors/gui/command/syntax/CommandSyntax.java
@@ -118,6 +118,7 @@ public abstract class CommandSyntax {
 		registerCommandSyntax("difficulty", SyntaxDifficulty.class);
 		registerCommandSyntax("defaultgamemode", SyntaxDefaultGamemode.class);
 		registerCommandSyntax("kill", SyntaxKill.class);
+		registerCommandSyntax("toggledownfall", SyntaxNoArguments.class);
 		registerCommandSyntax("give", SyntaxGive.class);
 		registerCommandSyntax("scoreboard", SyntaxScoreboard.class);
 	}
---
+++ src/main/java/net/earthcomputer/easyeditors/gui/command/syntax/SyntaxNoArguments.java
@@ -0,0 +1,12 @@
+package net.earthcomputer.easyeditors.gui.command.syntax;
+
+import net.earthcomputer.easyeditors.gui.command.slot.IGuiCommandSlot;
+
+public class SyntaxNoArguments extends CommandSyntax {
+
+	@Override
+	public IGuiCommandSlot[] setupCommand() {
+		return new IGuiCommandSlot[0];
+	}
+
+}
