dc9adaf80aec7ed5faa856fd1e3e993ff7864ee4
Earthcomputer <burtonjae@hotmail.co.uk>
2017-02-15 08:09:10 +0000

    Finish stats syntax

--- src/main/java/net/earthcomputer/easyeditors/gui/command/syntax/SyntaxStats.java
+++ src/main/java/net/earthcomputer/easyeditors/gui/command/syntax/SyntaxStats.java
@@ -14,6 +14,7 @@ import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotPlayerSelector;
 import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotRadioList;
 import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotRectangle;
 import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotRelativeCoordinate;
+import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotVerticalArrangement;
 import net.earthcomputer.easyeditors.gui.command.slot.IGuiCommandSlot;
 import net.earthcomputer.easyeditors.util.Translate;
 import net.minecraft.client.resources.I18n;
@@ -25,7 +26,11 @@ public class SyntaxStats extends CommandSyntax {
 	private CommandSlotRadioList source;
 	private CommandSlotRelativeCoordinate sourceBlock;
 	private CommandSlotPlayerSelector sourceEntity;
+	private CommandSlotMenu mode;
 	private CommandSlotMenu commandResultType;
+	private CommandSlotModifiable<CommandSlotVerticalArrangement> modeParam;
+	private CommandSlotVerticalArrangement setModeParam;
+	private CommandSlotVerticalArrangement clearModeParam;
 	private CommandSlotPlayerSelector targetEntity;
 	private CommandSlotModifiable<IGuiCommandSlot> modifiableTargetObjective;
 	private CommandSlotMenu targetObjective;
@@ -62,7 +67,8 @@ public class SyntaxStats extends CommandSyntax {
 				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_REPLACEITEM_TARGET_BLOCK,
 						Colors.miscBigBoxLabel.color, sourceBlock),
 				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_REPLACEITEM_TARGET_ENTITY,
-						Colors.miscBigBoxLabel.color, sourceEntity)) {
+						Colors.miscBigBoxLabel.color,
+						new CommandSlotRectangle(sourceEntity, Colors.playerSelectorBox.color))) {
 			@Override
 			protected int getSelectedIndexForString(String[] args, int index) throws CommandSyntaxException {
 				// This was offset by one in the overridden version of
@@ -101,6 +107,20 @@ public class SyntaxStats extends CommandSyntax {
 			}
 		};
 
+		mode = new CommandSlotMenu(new String[] { Translate.GUI_COMMANDEDITOR_STATS_MODE_SET,
+				Translate.GUI_COMMANDEDITOR_STATS_MODE_CLEAR }, "set", "clear") {
+			@Override
+			protected void onChanged(String to) {
+				if ("set".equals(to)) {
+					modeParam.setChild(setModeParam);
+				} else if ("clear".equals(to)) {
+					modeParam.setChild(clearModeParam);
+				} else {
+					throw new IllegalStateException();
+				}
+			}
+		};
+
 		CommandResultStats.Type[] resultTypes = CommandResultStats.Type.values();
 		String[] resultTypeTranslations = new String[resultTypes.length];
 		String[] resultTypeNames = new String[resultTypes.length];
@@ -112,16 +132,26 @@ public class SyntaxStats extends CommandSyntax {
 
 		targetEntity = new CommandSlotPlayerSelector(CommandSlotPlayerSelector.ONE_ONLY);
 
+		setModeParam = new CommandSlotVerticalArrangement(
+				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_STATS_TARGETENTITY,
+						Translate.GUI_COMMANDEDITOR_STATS_TARGETENTITY_TOOLTIP,
+						new CommandSlotRectangle(targetEntity, Colors.playerSelectorBox.color)),
+				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_STATS_TARGETOBJECTIVE,
+						Translate.GUI_COMMANDEDITOR_STATS_TARGETOBJECTIVE_TOOLTIP, modifiableTargetObjective));
+
+		clearModeParam = new CommandSlotVerticalArrangement();
+
+		modeParam = new CommandSlotModifiable<CommandSlotVerticalArrangement>(setModeParam);
+
 		return new IGuiCommandSlot[] {
 				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_STATS_SOURCE,
 						Translate.GUI_COMMANDEDITOR_STATS_SOURCE_TOOLTIP,
 						new CommandSlotRectangle(source, Colors.miscBigBoxBox.color)),
+				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_STATS_MODE,
+						Translate.GUI_COMMANDEDITOR_STATS_MODE_TOOLTIP, mode),
 				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_STATS_TYPE,
 						Translate.GUI_COMMANDEDITOR_STATS_TYPE_TOOLTIP, commandResultType),
-				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_STATS_TARGETENTITY,
-						Translate.GUI_COMMANDEDITOR_STATS_TARGETENTITY_TOOLTIP, targetEntity),
-				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_STATS_TARGETOBJECTIVE,
-						Translate.GUI_COMMANDEDITOR_STATS_TARGETOBJECTIVE_TOOLTIP, modifiableTargetObjective) };
+				modeParam };
 	}
 
 }
