b6635e8f0397ead5a410e968806989926392b49a
Earthcomputer <burtonjae@hotmail.co.uk>
2017-03-01 16:34:38 +0000

    Fix max string length in me syntax

--- src/main/java/net/earthcomputer/easyeditors/gui/command/syntax/SyntaxEmote.java
+++ src/main/java/net/earthcomputer/easyeditors/gui/command/syntax/SyntaxEmote.java
@@ -4,15 +4,22 @@ import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotFormattedTextFi
 import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotLabel;
 import net.earthcomputer.easyeditors.gui.command.slot.CommandSlotTextField;
 import net.earthcomputer.easyeditors.gui.command.slot.IGuiCommandSlot;
+import net.earthcomputer.easyeditors.gui.command.slot.ITextField;
 import net.earthcomputer.easyeditors.util.Translate;
 
 public class SyntaxEmote extends CommandSyntax {
 
 	@Override
 	public IGuiCommandSlot[] setupCommand() {
+		ITextField<?> textField;
+		if (getContext().canHoldFormatting()) {
+			textField = new CommandSlotFormattedTextField(500);
+		} else {
+			textField = new CommandSlotTextField(500, 500);
+		}
+		textField.setMaxStringLength(Short.MAX_VALUE);
 		return new IGuiCommandSlot[] {
-				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_ME_MESSAGE, getContext().canHoldFormatting()
-						? new CommandSlotFormattedTextField(500) : new CommandSlotTextField(500, 500)) };
+				CommandSlotLabel.createLabel(Translate.GUI_COMMANDEDITOR_ME_MESSAGE, (IGuiCommandSlot) textField) };
 	}
 
 }
